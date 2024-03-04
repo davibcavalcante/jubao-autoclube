52961df7ac3542e552db64ded65a9584bb715f0a
davibcavalcante <davib506@gmail.com>
2024-03-04 09:03:49 -0300

    Atualização diária de notícias

--- api/v1/middlewares/noticias.js
+++ api/v1/middlewares/noticias.js
@@ -20,6 +20,13 @@ const createNews = (url, title, date, external = false) => {
 module.exports.getNews = (pageNumber, pageSize) => {
     const newsArray = [
         createNews(
+            'https://cba.org.br/upload/noticias/2024/2688/thumb/pioneiro-e-visionario-fernando-monteiro-um-dos-principais-responsaveis-pela-pratica-do-automobilismo-no-nordeste-completa-80-anos.png',
+            'Pioneiro e visionário, Fernando Monteiro, um dos principais responsáveis pela prática do automobilismo no Nordeste, completa 80 anos',
+            '03 Março 2024',
+            'https://cba.org.br/noticias/noticiasinfo/2688/pioneiro-e-visionario-fernando-monteiro-um-dos-principais-responsaveis-pela-pratica-do-automobilismo-no-nordeste-completa-80-anos'
+        ),
+
+        createNews(
             'https://cba.org.br/upload/noticias/2024/2687/thumb/tudo-pronto-para-o-sul-brasileiro-de-kart-em-pato-branco-pr-.png',
             'Tudo pronto para o Sul-Brasileiro de Kart, em Pato Branco (PR)',
             '28 Fevereiro 2024',
